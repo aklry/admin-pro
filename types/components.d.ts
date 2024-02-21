2698816e7153dd4b8293a3ccadf65a4bc6b04a7a
Aklry <74409739+aklry@users.noreply.github.com>
2024-02-21 22:47:58 +0800

    style(代码格式化): 修复代码格式

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,11 +7,11 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    ElButton: (typeof import('element-plus/es'))['ElButton']
+    ElForm: (typeof import('element-plus/es'))['ElForm']
+    ElFormItem: (typeof import('element-plus/es'))['ElFormItem']
+    ElInput: (typeof import('element-plus/es'))['ElInput']
+    RouterLink: (typeof import('vue-router'))['RouterLink']
+    RouterView: (typeof import('vue-router'))['RouterView']
   }
 }
