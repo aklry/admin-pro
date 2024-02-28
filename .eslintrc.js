19d2e759aa49cb1986327adc77b47da5386b8d1d
Aklry <74409739+aklry@users.noreply.github.com>
2024-02-28 19:05:08 +0800

    fix(解决bug): 书写正确的路径

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    './eslintrc-auto-import.json',
+    './.eslintrc-auto-import.json',
   ],
   plugins: ['vue', '@typescript-eslint'],
   /*
