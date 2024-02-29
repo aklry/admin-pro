a9e47099efdfd48c7a9cd692fd5a26f48058d776
aklry <3265242862@qq.com>
2024-02-29 15:05:09 +0800

    ci(): 修改stylelint配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -51,6 +51,17 @@ module.exports = {
     ],
     // 允许对应内核前缀
     'property-no-vendor-prefix': null,
+    'block-closing-brace-empty-line-before': null,
+    'block-closing-brace-newline-after': null,
+    'block-closing-brace-newline-before': null,
+    'block-closing-brace-space-before': null,
+    'block-opening-brace-newline-after': null,
+    'block-opening-brace-space-after': null,
+    'block-opening-brace-space-before': null,
+    'declaration-block-semicolon-newline-after': null,
+    'declaration-block-semicolon-space-after': null,
+    'declaration-block-semicolon-space-before': null,
+    'declaration-block-trailing-semicolon': null,
     // 指定样式的排序 修复后会帮我们自动整理CSS样式的顺序
     'order/properties-order': [
       'position',
