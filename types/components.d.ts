3993012e860676aa55c0be00bad1566bfb3900d4
Aklry <74409739+aklry@users.noreply.github.com>
2024-03-02 23:24:33 +0800

    style: 代码格式化

--- types/components.d.ts
+++ types/components.d.ts
@@ -8,6 +8,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElAside: (typeof import('element-plus/es'))['ElAside']
+    ElBreadcrumb: (typeof import('element-plus/es'))['ElBreadcrumb']
+    ElBreadcrumbItem: (typeof import('element-plus/es'))['ElBreadcrumbItem']
     ElButton: (typeof import('element-plus/es'))['ElButton']
     ElContainer: (typeof import('element-plus/es'))['ElContainer']
     ElDialog: (typeof import('element-plus/es'))['ElDialog']
@@ -22,6 +24,7 @@ declare module 'vue' {
     ElOption: (typeof import('element-plus/es'))['ElOption']
     ElPagination: (typeof import('element-plus/es'))['ElPagination']
     ElSelect: (typeof import('element-plus/es'))['ElSelect']
+    ElSubMenu: (typeof import('element-plus/es'))['ElSubMenu']
     ElTable: (typeof import('element-plus/es'))['ElTable']
     ElTableColumn: (typeof import('element-plus/es'))['ElTableColumn']
     ElTree: (typeof import('element-plus/es'))['ElTree']
