0c16bc8e49e11e19eb7c5023b0107bd11a3a93b2
Aklry <74409739+aklry@users.noreply.github.com>
2024-03-03 15:17:41 +0800

    ci(eslint): 添加eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,5 +53,6 @@ module.exports = {
     'vue/script-setup-uses-vars': 'error', // 防止<script setup>使用的变量<template>被标记为未使用
     'vue/no-mutating-props': 'off', // 不允许组件 prop的改变
     'vue/attribute-hyphenation': 'off', // 对模板中的自定义组件强制执行属性命名样式
+    'no-constant-condition': 'off', // 禁止在条件中使用常量表达式
   },
 }
